792471c0c58f5eb88754b7b0a6bad49faa04e116
PratikDhanave <i.pratikdhanave@gmail.com>
2023-09-30 12:01:18 +0530

    trap error added

--- vm/vm.go
+++ vm/vm.go
@@ -42,12 +42,18 @@ type vm struct {
 }
 
 func (m *vm) Invoke(name string, arguments ...interpreter.Value) (interpreter.Value, error) {
+
+	// GetExport attempts to find an export on this instance by 'name'
+	// May return `nil` if this instance has no export named `name`
+
 	f := m.instance.GetExport(m.store, name).Func()
 
 	rawArguments := make([]any, len(arguments))
 	for i, argument := range arguments {
 		rawArguments[i] = argument
 	}
+
+	// Call invokes this function with the provided `args`.
 
 	res, err := f.Call(m.store, rawArguments...)
 	if err != nil {
@@ -68,90 +74,145 @@ func NewVM(wasm []byte) (VM, error) {
 		return nil, err
 	}
 
+	// NewConfig creates a new `Config` with all default options configured.
+
 	config := wasmtime.NewConfig()
+
+	// SetWasmReferenceTypes configures whether the wasm reference types proposal is enabled.
 	config.SetWasmReferenceTypes(true)
+
+	// NewEngineWithConfig creates a new `Engine` with the `Config` provided
+	// Note that once a `Config` is passed to this method it cannot be used again.
 
 	engine := wasmtime.NewEngineWithConfig(config)
 
 	store := wasmtime.NewStore(engine)
+
+	// Module is module which collects
+	// definations for types, functions, tables, memories and globals.
+	// In addition ,it can declare imports and exports
+	// and provide initialization logic
+	// in the form of data and element segments or a start function.
+	// Module organized WebAssembly programs as the unit of deployment,
+	// loading and compilation.
 
 	module, err := wasmtime.NewModule(store.Engine, wasm)
 	if err != nil {
 		return nil, err
 	}
 
+	// WrapFunc wraps a native go function, `f` as a wasm `func`.
+
+	// This function differs from `NewFunc` in that it will determine
+	// the type signature of the wasm function given the
+	// input value of `f`.
+	// The value `f` provided must be a Go function.
+	// It may take any number of the following type as arguments :
+
+	// `int32` - a wasm `i32`
+
+	// `int64` a wasm `i64`
+
+	// `float32`
+
+	// `float64`
+
+	// `*Caller`
+
+	//	`*Func`
+
+	// anything else - a wasm `extenref`
+
+	// The go function may return  any number of values.
+
+	// It can return any number of primitive wasm values (integers/floats),
+	// and the last return value may optionally be `*Trap` returned is nil
+	// then the others values are returned from the wasm function.
+	// Otherwise the `*Trap` is returned and
+	// it is consider as if the host function traped
+
+	// if the function `f` panics then the panic will be propagated to the caller.
+
+	initfn := func(caller *wasmtime.Caller, offset int32, length int32) (any, *wasmtime.Trap) {
+		if offset < 0 {
+			return nil, wasmtime.NewTrap(fmt.Sprintf("Int: invalid offset: %d", offset))
+		}
+
+		if length < 2 {
+			return nil, wasmtime.NewTrap(fmt.Sprintf("Int: invalid length: %d", length))
+		}
+
+		mem := caller.GetExport("mem").Memory()
+
+		bytes := C.GoBytes(mem.Data(store), C.int(length))
+
+		value := new(big.Int).SetBytes(bytes[1:])
+		if bytes[0] == 0 {
+			value = value.Neg(value)
+		}
+
+		return interpreter.NewUnmeteredIntValueFromBigInt(value), nil
+	}
+
 	intFunc := wasmtime.WrapFunc(
 		store,
-		func(caller *wasmtime.Caller, offset int32, length int32) (any, *wasmtime.Trap) {
-			if offset < 0 {
-				return nil, wasmtime.NewTrap(fmt.Sprintf("Int: invalid offset: %d", offset))
-			}
-
-			if length < 2 {
-				return nil, wasmtime.NewTrap(fmt.Sprintf("Int: invalid length: %d", length))
-			}
-
-			mem := caller.GetExport("mem").Memory()
-
-			bytes := C.GoBytes(mem.Data(store), C.int(length))
-
-			value := new(big.Int).SetBytes(bytes[1:])
-			if bytes[0] == 0 {
-				value = value.Neg(value)
-			}
-
-			return interpreter.NewUnmeteredIntValueFromBigInt(value), nil
-		},
+		initfn,
 	)
+
+	stringfn := func(caller *wasmtime.Caller, offset int32, length int32) (any, *wasmtime.Trap) {
+		if offset < 0 {
+			return nil, wasmtime.NewTrap(fmt.Sprintf("String: invalid offset: %d", offset))
+		}
+
+		if length < 0 {
+			return nil, wasmtime.NewTrap(fmt.Sprintf("String: invalid length: %d", length))
+		}
+
+		mem := caller.GetExport("mem").Memory()
+
+		bytes := C.GoBytes(mem.Data(store), C.int(length))
+
+		return interpreter.NewUnmeteredStringValue(string(bytes)), nil
+	}
 
 	stringFunc := wasmtime.WrapFunc(
 		store,
-		func(caller *wasmtime.Caller, offset int32, length int32) (any, *wasmtime.Trap) {
-			if offset < 0 {
-				return nil, wasmtime.NewTrap(fmt.Sprintf("String: invalid offset: %d", offset))
-			}
-
-			if length < 0 {
-				return nil, wasmtime.NewTrap(fmt.Sprintf("String: invalid length: %d", length))
-			}
-
-			mem := caller.GetExport("mem").Memory()
-
-			bytes := C.GoBytes(mem.Data(store), C.int(length))
-
-			return interpreter.NewUnmeteredStringValue(string(bytes)), nil
-		},
+		stringstringfn,
 	)
+
+	addfn := func(left, right any) (any, *wasmtime.Trap) {
+		leftNumber, ok := left.(interpreter.NumberValue)
+		if !ok {
+			return nil, wasmtime.NewTrap(fmt.Sprintf("add: invalid left: %#+v", left))
+		}
+
+		rightNumber, ok := right.(interpreter.NumberValue)
+		if !ok {
+			return nil, wasmtime.NewTrap(fmt.Sprintf("add: invalid right: %#+v", right))
+		}
+
+		return leftNumber.Plus(inter, rightNumber, interpreter.EmptyLocationRange), nil
+	}
 
 	addFunc := wasmtime.WrapFunc(
 		store,
-		func(left, right any) (any, *wasmtime.Trap) {
-			leftNumber, ok := left.(interpreter.NumberValue)
-			if !ok {
-				return nil, wasmtime.NewTrap(fmt.Sprintf("add: invalid left: %#+v", left))
-			}
-
-			rightNumber, ok := right.(interpreter.NumberValue)
-			if !ok {
-				return nil, wasmtime.NewTrap(fmt.Sprintf("add: invalid right: %#+v", right))
-			}
-
-			return leftNumber.Plus(inter, rightNumber, interpreter.EmptyLocationRange), nil
-		},
+		addfn,
 	)
 
 	// NOTE: wasmtime currently does not support specifying imports by name,
 	// unlike other WebAssembly APIs like wasmer, JavaScript, etc.,
 	// i.e. imports are imported in the order they are given.
 
+	imports := []wasmtime.AsExtern{
+		intFunc,
+		stringFunc,
+		addFunc,
+	}
+
 	instance, err := wasmtime.NewInstance(
 		store,
 		module,
-		[]wasmtime.AsExtern{
-			intFunc,
-			stringFunc,
-			addFunc,
-		},
+		imports,
 	)
 	if err != nil {
 		return nil, err
